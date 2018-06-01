d85bc9435be9049c2403b00891a038c82181fadc
HuangQingSong <qingsong.huang@igets.net>
2018-06-01 17:33:46 +0800

    new

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const router = require('koa-router')();
-
-router.get('/:name',async (ctx, next) => {
-    await ctx.render(ctx.params.name);
-});
-
-module.exports = router;
